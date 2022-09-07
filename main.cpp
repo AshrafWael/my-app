76bd3526e667274d71e4218efe2fdce165c8d94a
ashrafwael <ashrafwael582@gmail.com>
2022-09-07 16:40:08 +0200

    create new commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      //cout <<"my name is ashraf"<< endl ;
+      cout<<"*" <<endl
+      <<"**" <<endl
+       <<"***"<<endl
+      <<"*****"<< endl ;
+
+
+    return 0;
+}
